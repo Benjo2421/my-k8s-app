2319fb056665bae70d0591bcaa4cd749308abdb4
benjo2421 <leducb10@gmail.com>
2025-05-02 13:55:04 -0400

    essai 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('esssay de test\n');
+    res.end('esssay de 2\n');
 });
 
 

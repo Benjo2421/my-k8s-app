d180631c5d8643a20a503eabb98f92b46c26c35d
benjo2421 <leducb10@gmail.com>
2025-05-02 14:20:35 -0400

    essai sa marche

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('ceci est un test merge \n');
+    res.end('you sa marche \n');
 });
 
 
